fd4d6e57d447715abf2db86058853ae7f8d843dd
ppaushya <43550473+ppaushya@users.noreply.github.com>
2018-10-04 17:01:22 +0530

    Update Validator.java

--- AssignmentOct01/AssignmentOct01/util/Validator.java
+++ AssignmentOct01/AssignmentOct01/util/Validator.java
@@ -4,7 +4,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
-
 	public static boolean validateName(String name) {
 		return validateVerbalData(name);
 	}
@@ -15,6 +14,12 @@ public class Validator {
 	
 	public static boolean validateState(String state) {
 		return validateVerbalData(state);
+	}
+	
+	public static boolean validateAccountNumber(String accountNumberString) {
+		Pattern namePattern = Pattern.compile("\\d{6,10}");
+		Matcher nameMatcher = namePattern.matcher(accountNumberString);
+		return nameMatcher.matches();
 	}
 	
 	public static boolean validateVerbalData(String str) {
@@ -35,9 +40,16 @@ public class Validator {
 		return nameMatcher.matches();
 	}
 	
+	public static boolean validateDate(String dateString) {
+//		Pattern namePattern = Pattern.compile("\\d{2}-\\d{2}-\\d{4}");
+		Pattern namePattern = Pattern.compile("([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])[-]([1-9]|0[1-9]|1[0-2])[-][1-2][0-9]{3}");
+		Matcher nameMatcher = namePattern.matcher(dateString);
+		return nameMatcher.matches();
+	}
+	
 	public static void main(String[] args) {
 		//System.out.println(validateName("Ketan Chauhan"));
-		
+		System.out.println(validateEmail("Ketan@capgemini.com"));
 		/*System.out.println(validateEmail("Ketan.Chauhan@capgemini.com"));
 		System.out.println(validateEmail("Ketan@capgemini.com"));
 		System.out.println(validateEmail("KetanChauhan.@capgemini.com"));
@@ -50,9 +62,11 @@ public class Validator {
 		System.out.println(validateEmail("@capgemini.com"));
 		System.out.println(validateEmail("Ketan.Chauhan"));*/
 		
-		System.out.println(validateMobileNumber("0123456789"));
+		/*System.out.println(validateMobileNumber("0123456789"));
 		System.out.println(validateMobileNumber("012345678"));
 		System.out.println(validateMobileNumber("0123456789a"));
-		System.out.println(validateMobileNumber("01234 56789"));
+		System.out.println(validateMobileNumber("01234 56789"));*/
+		
+//		System.out.println(validateDate("04/12/2018"));
 	}
 }
