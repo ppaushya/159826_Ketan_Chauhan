f82a3d88d74a41a97e6c2eb7b1ff4c27cc6a2b70
ppaushya <43550473+ppaushya@users.noreply.github.com>
2018-10-04 09:19:22 +0530

    Add files via upload

---
+++ AssignmentOct01/AssignmentOct01/main/Account.java
@@ -0,0 +1,84 @@
+package main;
+
+import java.time.LocalDate;
+
+import util.AccountType;
+
+public class Account {
+	
+	private long accountNo;
+	private AccountType accountType;
+	private LocalDate openingDate;
+	private double openingBalance;
+	
+	public Account() {
+		super();
+	}
+	
+	public Account(long accountNo, AccountType accountType, LocalDate openingDate, double openingBalance) {
+		super();
+		this.accountNo = accountNo;
+		this.accountType = accountType;
+		this.openingDate = openingDate;
+		this.openingBalance = openingBalance;
+	}
+	
+	public long getAccountNo() {
+		return accountNo;
+	}
+	
+	public void setAccountNo(long accountNo) {
+		this.accountNo = accountNo;
+	}
+	
+	public AccountType getAccountType() {
+		return accountType;
+	}
+	
+	public void setAccountType(AccountType accountType) {
+		this.accountType = accountType;
+	}
+	
+	public LocalDate getOpeningDate() {
+		return openingDate;
+	}
+	
+	public void setOpeningDate(LocalDate openingDate) {
+		this.openingDate = openingDate;
+	}
+	
+	public double getOpeningBalance() {
+		return openingBalance;
+	}
+	
+	public void setOpeningBalance(double openingBalance) {
+		this.openingBalance = openingBalance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [accountNo=" + accountNo + ", accountType=" + accountType + ", openingDate=" + openingDate
+				+ ", openingBalance=" + openingBalance + "]";
+	}
+	
+	/*public double getCurrentBalance() {
+		//TODO 
+		return openingBalance;
+	}*/
+	
+	public double getCurrentBalanceAfterTransactions(Transaction[] transactions) {
+		double balance = openingBalance;
+		for(int i=0;i<transactions.length;i++) {
+			if(transactions[i]!=null) {
+				if(transactions[i].getTransactionType().equals("Debit")) {
+					balance -= transactions[i].getAmount();
+				}else if(transactions[i].getTransactionType().equals("Credit")) {
+					balance += transactions[i].getAmount();
+				}
+			}
+		}
+		return balance;
+	}
+	
+	
+}
