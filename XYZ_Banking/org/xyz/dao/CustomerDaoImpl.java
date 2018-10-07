49a394dd1a4243f59396670f51616b937fc02eb8
ppaushya <43550473+ppaushya@users.noreply.github.com>
2018-10-07 19:24:05 +0530

    Update CustomerDaoImpl.java

--- XYZ_Banking/org/xyz/dao/CustomerDaoImpl.java
+++ XYZ_Banking/org/xyz/dao/CustomerDaoImpl.java
@@ -17,19 +17,19 @@ public class CustomerDaoImpl implements ICustomerDao{
 		List<Customer> customers = new ArrayList<Customer>();
 		
 		Address address1 = new Address("North Avenue","Cross St","Chennai","IN","122345");
-		customers.add(new Customer(IdGenerator.generateAccountNumber(),"Tom","Smith",LocalDate.of(1985, 3, 15),"tom@mail.com","1234567890",address1));
+		customers.add(new Customer(IdGenerator.generateCustomerNumber(),"Tom","Smith",LocalDate.of(1985, 3, 15),"tom@mail.com","1234567890",address1));
 		
 		Address address2 = new Address("South Avenue","Cross St","Chennai","IN","122345");
-		customers.add(new Customer(IdGenerator.generateAccountNumber(),"Jerry","Bill",LocalDate.of(2005, 9, 1),"jerry@mail.com","7519243565",address2));
+		customers.add(new Customer(IdGenerator.generateCustomerNumber(),"Jerry","Bill",LocalDate.of(2005, 9, 1),"jerry@mail.com","7519243565",address2));
 		
 		Address address3 = new Address("West car st","Cross St","Chennai","IN","122345");
-		customers.add(new Customer(IdGenerator.generateAccountNumber(),"Amit","Patel",LocalDate.of(1990, 4, 30),"amit@mail.com","7583692140",address3));
+		customers.add(new Customer(IdGenerator.generateCustomerNumber(),"Amit","Patel",LocalDate.of(1990, 4, 30),"amit@mail.com","7583692140",address3));
 		
 		Address address4 = new Address("North Avenue","Cross St","Pune","IN","122345");
-		customers.add(new Customer(IdGenerator.generateAccountNumber(),"Bharat","Shah",LocalDate.of(1995, 5, 5),"bharat@mail.com","1234567890",address4));
+		customers.add(new Customer(IdGenerator.generateCustomerNumber(),"Bharat","Shah",LocalDate.of(1995, 5, 5),"bharat@mail.com","1234567890",address4));
 		
 		Address address5 = new Address("North Avenue","Cross St","Delhi","IN","122345");
-		customers.add(new Customer(IdGenerator.generateAccountNumber(),"Chetan","Parmar",LocalDate.of(2000, 12, 19),"chetan@mail.com","0312456789",address5));
+		customers.add(new Customer(IdGenerator.generateCustomerNumber(),"Chetan","Parmar",LocalDate.of(2000, 12, 19),"chetan@mail.com","0312456789",address5));
 		
 		return customers;
 	}
@@ -39,8 +39,18 @@ public class CustomerDaoImpl implements ICustomerDao{
 	}
 
 	public void createCustomer(Customer customer) {
+		customer.setCustomerId(IdGenerator.generateCustomerNumber());
+		customers.add(customer);
 		
-		
+	}
+
+	public Customer getCustomerFromCustomerId(long customerId) {
+		for(Customer customer:customers) {
+			if(customer.getCustomerId()==customerId) {
+				return customer;
+			}
+		}
+		return null;
 	}
 
 
